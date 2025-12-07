951544100ae361269d45997532887fb6587e178a
Rayen Lee <rayenlee64@gmail.com>
2025-12-08 09:06:25 +1300

    script to pull math out of parsed cellml component

---
+++ programs/retrievemath.py
@@ -0,0 +1,34 @@
+from libcellml import Parser, Validator
+from pathlib import Path
+
+cwd = Path.cwd()
+
+model_path = cwd.parent / "cellml/condensed_capillary"
+model_file_name = "capillary_network.cellml"
+
+def log_issues(libcellobj):
+    print(f'{libcellobj.issueCount()} issue/s found for {libcellobj.__class__}')
+    for i in range(0, libcellobj.issueCount()):
+        print(libcellobj.issue(i).description())
+
+def main():
+    print("Opening CellML file...")
+    with open(model_path / model_file_name) as f:
+        content = f.read()
+
+    print("Parsing CellML file...")
+    parser = Parser(False)
+    model = parser.parseModel(content)
+    log_issues(parser)
+
+    print("Validating model...")
+    validator = Validator()
+    validator.validateModel(model)
+    log_issues(validator)
+
+    for i in range(model.componentCount()):
+        component = model.component(i)
+        print(component.math())
+
+if __name__ == "__main__":
+    main()
